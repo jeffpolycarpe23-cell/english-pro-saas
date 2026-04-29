8f8e491715c05a70b84518267efeef5c62951db7
jeffpolycarpe23-cell <jeffpolycarpe23@gmail.com>
2026-04-29 19:29:04 -0400

    Integrate EnglishLearningSaaS component into App

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import EnglishLearningSaaS from './EnglishLearningSaaS';
+
+function App() {
+  return (
+    <div className="App">
+      <EnglishLearningSaaS />
+    </div>
+  );
+}
+
+export default App;
 
