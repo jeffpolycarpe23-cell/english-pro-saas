c38feb4ea60a6a195af81fe55a2697160a7eba76
jeffpolycarpe23-cell <jeffpolycarpe23@gmail.com>
2026-04-29 19:26:32 -0400

    Set up React application with EnglishLearningSaaS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import EnglishLearningSaaS from './EnglishLearningSaaS';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <EnglishLearningSaaS />
+  </React.StrictMode>
+);
